a61ce4ef4995bdb1bed0e58e51556efdb26ea53d
Meghna Dubey <71982319+Meg-D@users.noreply.github.com>
2021-11-19 00:14:05 +0530

    Update Employees.java

--- src/main/java/com/example/erp/bean/Employees.java
+++ src/main/java/com/example/erp/bean/Employees.java
@@ -1,4 +1,75 @@
 package com.example.erp.bean;
 
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table
 public class Employees {
+    @Id
+    private int emp_id;
+
+    @Column
+    private int name;
+
+    @Column
+    private String dob;
+
+    @ManyToOne
+    @JoinColumn(name="dept_id")
+    private Departments departments;
+
+    @ManyToMany
+    private List<Projects> projectsList;
+
+    public Employees() {
+    }
+
+    public Employees(int emp_id, int name, String dob, Departments departments, List<Projects> projectsList) {
+        this.emp_id = emp_id;
+        this.name = name;
+        this.dob = dob;
+        this.departments = departments;
+        this.projectsList = projectsList;
+    }
+
+    public int getEmp_id() {
+        return emp_id;
+    }
+
+    public void setEmp_id(int emp_id) {
+        this.emp_id = emp_id;
+    }
+
+    public int getName() {
+        return name;
+    }
+
+    public void setName(int name) {
+        this.name = name;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public Departments getDepartments() {
+        return departments;
+    }
+
+    public void setDepartments(Departments departments) {
+        this.departments = departments;
+    }
+
+    public List<Projects> getProjectsList() {
+        return projectsList;
+    }
+
+    public void setProjectsList(List<Projects> projectsList) {
+        this.projectsList = projectsList;
+    }
 }
