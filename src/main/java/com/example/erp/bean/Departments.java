6eb1fe288bb6b57f3c08e413a194deef44f98d2a
Meghna Dubey <71982319+Meg-D@users.noreply.github.com>
2021-11-19 00:13:35 +0530

    Update Departments.java

--- src/main/java/com/example/erp/bean/Departments.java
+++ src/main/java/com/example/erp/bean/Departments.java
@@ -1,4 +1,63 @@
 package com.example.erp.bean;
 
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name="dept")
 public class Departments {
+
+    @Id
+    private int dept_id;
+
+    @Column(name="dept_name")
+    private String name;
+
+    @Column
+    private String address;
+
+    @OneToMany
+    private List<Employees> employeesList;
+
+    public Departments() {
+    }
+
+    public Departments(int dept_id, String name, String address, List<Employees> employeesList) {
+        this.dept_id = dept_id;
+        this.name = name;
+        this.address = address;
+        this.employeesList = employeesList;
+    }
+
+    public int getDept_id() {
+        return dept_id;
+    }
+
+    public void setDept_id(int dept_id) {
+        this.dept_id = dept_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public List<Employees> getEmployeesList() {
+        return employeesList;
+    }
+
+    public void setEmployeesList(List<Employees> employeesList) {
+        this.employeesList = employeesList;
+    }
 }
